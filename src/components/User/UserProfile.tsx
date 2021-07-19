d3ecdf4b9e9dee719fabd68d0f954b694252817c
kacper <kacykvaa@gmail.com>
2021-07-19 14:49:03 +0200

    style userProfile  add styling to game stats

--- src/components/User/UserProfile.tsx
+++ src/components/User/UserProfile.tsx
@@ -56,6 +56,12 @@ const About = styled.div`
     border-radius: 10rem;
   }
 
+  span {
+    color: green;
+    font-weight: bold;
+  }
+}
+
 @media${device.tablet} {
   max-width: 35rem;
 `
@@ -89,8 +95,8 @@ export const UserProfile = () => {
                         <img src={avatarAvocado} alt="Avocado Avatar"/>
                         <p>Name: {user.name}</p>
                         <p>Mail: {user.email} </p>
-                        <p>Total Games: {user.totalGames || '0'}</p>
-                        <p>Points Scored: {user.pointsScored || '0'} </p>
+                        <p>Total Games: <span>{user.totalGames}</span></p>
+                        <p>Points Scored: <span>{user.pointsScored}</span></p>
                     </About>
                     <Button
                         onClick={logoutHandler}
